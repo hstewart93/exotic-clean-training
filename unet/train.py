4549addef10bf92396c520ad27673b5cbc1ffff8
hstewart93 <harriet.stt@gmail.com>
2025-08-22 15:58:44 +0100

    add contaminants on batch to clean simulations.

--- unet/train.py
+++ unet/train.py
@@ -6,10 +6,116 @@ from datetime import datetime
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import Dataset, DataLoader
 from matplotlib import pyplot as plt
+from torchvision import transforms as tfs
+import random
 
 from unet.models import CustomCallback, UNet
+
+
+class CustomImageDataset(Dataset):
+    def __init__(self, imgs, transform=None, target_transform=None):
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        image = self.imgs[idx]
+        if self.transform:
+            image, label = add_contaminants.forward(image)
+            image, label = pre_process_image.forward(image, label)
+        return image, label
+
+
+class PreprocessImage(torch.nn.Module):
+    def forward(self, img=None, label=None, normalise_max=300):
+
+        if img is None:
+            assert False
+        if label is None:
+            assert False
+
+        data_max = torch.max(img)
+        normalisation_factor = normalise_max / data_max
+        img_normalised = img * normalisation_factor
+        label_normalised = label * normalisation_factor
+
+        return torch.log1p(img_normalised), torch.log1p(label_normalised)
+
+
+class AddContaminants(torch.nn.Module):
+    def forward(self, img=None, label=None, Jmag_range=(1, 16)):
+
+        if img is None:
+            assert False
+
+        # # Add the random order 0s
+        # print("within addContaminants")
+        # print("img shape:", img.shape)
+        # print("order0 shape:", order_zeros.shape)
+
+        num_spectra, target_rows, target_cols = img.shape
+        num_order0s, array_rows, array_cols = order_zeros.shape
+        contam_list = []
+        N_contaminants = random.randint(0, 10)
+
+        new_img = img
+        new_label = torch.zeros(img.shape)
+
+        for _ in range(N_contaminants):
+
+            # Randomize the top-left position of the array of ones
+            start_row = random.randint(-array_rows + 1, target_rows)
+            start_col = random.randint(700, target_cols)  # POM starts at col 700
+
+            # Randomly choose a multiplication factor from the specified range
+            factor = random.uniform(*Jmag_range)
+
+            order_ = random.randint(0, num_order0s - 1)
+
+            contaminant = (
+                order_zeros[order_, :, :] * 1e4
+            )  # Scaling factor based on observations
+
+            # Determine overlap region in the target array
+            end_row = start_row + array_rows
+            end_col = start_col + array_cols
+
+            # Clip indices to be within the bounds of the target array
+            target_start_row = max(start_row, 0)
+            target_start_col = max(start_col, 0)
+            target_end_row = min(end_row, target_rows)
+            target_end_col = min(end_col, target_cols)
+
+            # Determine the corresponding region of the array of ones
+            array_start_row = max(0, -start_row)
+            array_start_col = max(0, -start_col)
+            array_end_row = array_start_row + (target_end_row - target_start_row)
+            array_end_col = array_start_col + (target_end_col - target_start_col)
+
+            new_img[
+                :, target_start_row:target_end_row, target_start_col:target_end_col
+            ] += (
+                factor
+                * contaminant[
+                    array_start_row:array_end_row, array_start_col:array_end_col
+                ]
+            )
+
+            new_label[
+                :, target_start_row:target_end_row, target_start_col:target_end_col
+            ] += (
+                factor
+                * contaminant[
+                    array_start_row:array_end_row, array_start_col:array_end_col
+                ]
+            )
+            # print("end of loop")
+        return new_img, new_label
 
 
 def train_model(
@@ -147,30 +253,35 @@ print(f"Using device: {device}")
 if torch.cuda.is_available():
     print(f"GPU: {torch.cuda.get_device_name(0)}")
 
+add_contaminants = AddContaminants()
+pre_process_image = PreprocessImage()
 
 # Load data
-data_train = np.load(os.path.join(ROOT_DIR, "data_train.npy"))
-labels_train = np.load(os.path.join(ROOT_DIR, "labels_train.npy"))
+data_train = np.load(os.path.join(ROOT_DIR, "clean_train.npy"))
+data_train = data_train[:800, :, :]
+data_validation = np.load(os.path.join(ROOT_DIR, "clean_validation.npy"))
+data_validation = data_validation[:100, :, :]
 
-data_validation = np.load(os.path.join(ROOT_DIR, "data_validation.npy"))
-labels_validation = np.load(os.path.join(ROOT_DIR, "labels_validation.npy"))
+# Load order zeros
+order_zeros = np.load(os.path.join(ROOT_DIR, "contaminants.npy"))
 
 print(f"Data train shape: {data_train.shape}")
-print(f"Labels train shape: {labels_train.shape}")
 print(f"Data validation shape: {data_validation.shape}")
-print(f"Labels validation shape: {labels_validation.shape}")
+print(f"Order zero shape: {order_zeros.shape}")
+
+data_train = torch.FloatTensor(data_train).unsqueeze(-1)
+data_validation = torch.FloatTensor(data_validation).unsqueeze(-1)
 
 # Convert numpy arrays to PyTorch tensors
 data_train = torch.FloatTensor(data_train).permute(
     0, 3, 1, 2
 )  # (N, H, W, C) -> (N, C, H, W)
-labels_train = torch.FloatTensor(labels_train).permute(0, 3, 1, 2)
 data_validation = torch.FloatTensor(data_validation).permute(0, 3, 1, 2)
-labels_validation = torch.FloatTensor(labels_validation).permute(0, 3, 1, 2)
 
+transforms = tfs.Compose([tfs.ToTensor()])
 # Create datasets and dataloaders
-train_dataset = TensorDataset(data_train, labels_train)
-val_dataset = TensorDataset(data_validation, labels_validation)
+train_dataset = CustomImageDataset(data_train, transform=transforms)
+val_dataset = CustomImageDataset(data_validation, transform=transforms)
 
 batch_size = args.batch_size
 train_loader = DataLoader(
